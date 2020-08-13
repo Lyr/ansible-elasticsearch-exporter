4e16862c9bcc371231e018ea3f295edd5b19581c
Lyr <yann.lopik@gmail.com>
2020-08-14 00:14:09 +0200

    Fix error

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,9 @@
 """Role testing files using testinfra."""
 
 def test_exporter_running_and_enabled(host):
-    exporter = host.service('elasticsearch_exporter')
-
+    exporter = host.service('elasticsearch-exporter')
+    assert exporter.is_running
     assert exporter.is_enabled
-
-
-def test_exporter_listening_http(host):
-    socket = host.socket('tcp://0.0.0.0:9108')
-    assert socket.is_listening
 
 
 def test_mgmt_user_authentication(host):
